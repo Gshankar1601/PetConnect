0838671877f09bc701801e1b4367e97c05b3c2e2
Gshankar1601 <gshankar1601@gmail.com>
2024-05-31 13:31:40 +0530

    vscode push

---
+++ PetConnectCode/web/notifications.js
@@ -0,0 +1,14 @@
+// web/notifications.js
+
+function showNotification(title, body) {
+    if (Notification.permission === 'granted') {
+      new Notification(title, { body });
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          new Notification(title, { body });
+        }
+      });
+    }
+  }
+  
